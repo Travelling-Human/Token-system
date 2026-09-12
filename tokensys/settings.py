490c258fa651d586bace402cc287c3bdcbb2c3a4
Travelling-Human <jokerlaugh10@gmail.com>
2026-09-13 00:55:54 +0530

    setting.py update

--- tokensys/settings.py
+++ tokensys/settings.py
@@ -155,3 +155,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if os.environ.get('CSRF_TRUSTED_ORIGINS') else []
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
